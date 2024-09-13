83ccbd31829f0333bcaba8adf1b724d13e6a6f3f
Shlok-OS <shlokarora62@gmail.com>
2024-09-13 22:45:54 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,21 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function deleteLast() {
+    let currentDisplay = document.getElementById('display').value;
+    document.getElementById('display').value = currentDisplay.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
